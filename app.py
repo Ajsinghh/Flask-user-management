e6c9b25b4449640b37f1b129eeabc84204d351e2
Ajay <ajsinghdhanda0207@gmail.com>
2024-12-08 23:47:15 +0530

    Integrated mysql database, created User class and created users route

--- app.py
+++ app.py
@@ -1,8 +1,20 @@
 from flask import Flask, render_template
-
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
+app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:Ajsingh@localhost/users"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    role = db.Column(db.String(50), nullable = False)
+    def __repr__(self):
+        return f"<User {self.name}>"
 
 
 @app.route('/')
@@ -13,7 +25,10 @@ def base():
 def hello():
     return render_template('hello.html')
 
-
+@app.route('/users')
+def users():
+    users = User.query.all()
+    return render_template('users.html',users=users)
 
 if __name__  == "__main__":
     app.run(debug= True)
