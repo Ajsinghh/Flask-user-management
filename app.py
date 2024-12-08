e741931a0b0fac51522680ae52a86a98effecd00
Ajay <ajsinghdhanda0207@gmail.com>
2024-12-08 23:35:33 +0530

    created hello route

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
 from flask import Flask, render_template
 
+
 app = Flask(__name__)
 
+
+
 @app.route('/')
+def base():
+    return render_template('base.html')
+
+@app.route('/hello')
 def hello():
-    return "hello world"
+    return render_template('hello.html')
+
+
 
 if __name__  == "__main__":
     app.run(debug= True)
